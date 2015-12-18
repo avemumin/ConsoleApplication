7707d3c2b6fb4182f661c6504a6b54cf5eea0746
Jacek Szczepański <jacogitproject@gmail.com>
2015-12-18 21:45:38 +0100

    dodanie zarysu menu (klasa abstrakcyjna i menu kontroler)

---
+++ Projekcik/ConsoleApplication1/Menu/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Menu
+{
+    class MenuController
+    {
+        List<MenuEntry> lista;
+        public string Text { get; set; }
+        public MenuController(string text)
+        {
+            Text = text;
+            lista = new List<MenuEntry>();
+        }
+        public void PrintMenu()
+        {
+            int position = 0;
+            Console.WriteLine("---- {0} ----", Text);
+
+            foreach (MenuEntry item in lista)
+            {
+                Console.WriteLine(" {0}) {1} ", position++, item.TextOnTitle);
+            }
+        }
+
+        public void StartProgram()
+        {
+            ConsoleKeyInfo someKey;
+            do
+            {
+                Console.Clear();
+                PrintMenu();
+                someKey = Console.ReadKey();
+
+            } while (true);
+        }
+
+
+    }
+}
---
+++ Projekcik/ConsoleApplication1/Menu/MenuEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Menu
+{
+    abstract class MenuEntry
+    {
+        public string TextOnTitle { get; set; }
+        public MenuEntry(string titleText)
+        {
+            TextOnTitle = titleText;
+        }
+    }
+}
